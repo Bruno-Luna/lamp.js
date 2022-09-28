5563995729d856fe73538d2de5b7866957394a51
Bruno Luna <luna.brsilva@gmail.com>
2022-09-28 06:39:42 -0300

    rafactor(app.js): função criada para desaclopar funcionalidade

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 let idHora = document.getElementById('hora')
+let hora = new Date
+
 const interruptorLigado = document.querySelector('.ligar')
 const interruptorDesligado = document.querySelector('.desligar')
 const black = 'black'
 
 const ligar = function(){
-        let img = document.getElementById('lampada').style.backgroundImage = 'url("img/ligada.jpg")'
+        document.getElementById('lampada').style.backgroundImage = 'url("img/ligada.jpg")'
         interruptorLigado.setAttribute('style', 'background-color: white;')
         interruptorDesligado.setAttribute('style', 'border: 2px solid white;')
 
-    }
+}
     
-    const desligar = function(){
-        let img = document.getElementById('lampada').style.backgroundImage = 'url("img/desligada.jpg")'
+const desligar = function(){
+        document.getElementById('lampada').style.backgroundImage = 'url("img/desligada.jpg")'
         interruptorDesligado.setAttribute('style', 'background-color: white;')
         interruptorLigado.setAttribute('style', 'border: 2px solid white;')
-    }
+}
 
-    const capturarHora = function(){
-        let hora = new Date
+
+const capturarHora = function(){
         idHora.textContent = 'Hora atual: ' + hora.getHours() + ':' + hora.getMinutes()
         idHora.style.color = black
-    
-        if(hora.getHours() >= 6 && hora.getHours() < 18){
-            return desligar()
-        }else if(hora.getHours() >= 18 || hora.getHours() < 6 ){
-            return ligar()
-        }
-    
+       verificadorHora()
+}
+
+const verificadorHora = function(){
+    if(hora.getHours() >= 6 && hora.getHours() < 18){
+        return desligar()
+    }else if(hora.getHours() >= 18 || hora.getHours() < 6 ){
+        return ligar()
     }
+}
 
 capturarHora()
