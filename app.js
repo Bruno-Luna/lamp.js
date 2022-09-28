ff6925b3de0a97a43b84ad442c89db1cd477d42d
Bruno Luna <luna.brsilva@gmail.com>
2022-09-28 06:50:53 -0300

    refactor(app.js): arrow functions implementada

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const interruptorLigado = document.querySelector('.ligar')
 const interruptorDesligado = document.querySelector('.desligar')
 const black = 'black'
 
-const ligar = function(){
+const ligar = () => {
         document.getElementById('lampada').style.backgroundImage = 'url("img/ligada.jpg")'
         interruptorLigado.setAttribute('style', 'background-color: white;')
         interruptorDesligado.setAttribute('style', 'border: 2px solid white;')
 
 }
     
-const desligar = function(){
+const desligar = () => {
         document.getElementById('lampada').style.backgroundImage = 'url("img/desligada.jpg")'
         interruptorDesligado.setAttribute('style', 'background-color: white;')
         interruptorLigado.setAttribute('style', 'border: 2px solid white;')
 }
 
 
-const capturarHora = function(){
+const capturarHora = () => {
         idHora.textContent = 'Hora atual: ' + hora.getHours() + ':' + hora.getMinutes()
         idHora.style.color = black
        verificadorHora()
 }
 
-const verificadorHora = function(){
+const verificadorHora = () => {
     if(hora.getHours() >= 6 && hora.getHours() < 18){
         return desligar()
     }else if(hora.getHours() >= 18 || hora.getHours() < 6 ){
