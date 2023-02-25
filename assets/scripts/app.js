e7e6c1491ef5d9cff86c5e7acd068512c997f966
Bruno-Luna <luna.brsilva@gmail.com>
2023-02-25 11:08:06 -0300

    refactor: alteração nas variáveis

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,30 @@
 let idHora = document.getElementById('hora')
-let hora = new Date
+let data = new Date()
 
 const interruptorLigado = document.querySelector('.ligar')
 const interruptorDesligado = document.querySelector('.desligar')
-const black = 'black'
+const lampada = document.querySelector('#lampada')
 
 const ligar = () => {
-        document.getElementById('lampada').style.backgroundImage = 'url("/assets/img/ligada.jpg")'
+        lampada.style.backgroundImage = 'url("/assets/img/ligada.jpg")'
         interruptorLigado.setAttribute('style', 'background-color: white;')
         interruptorDesligado.setAttribute('style', 'border: 2px solid white;')
 }
     
 const desligar = () => {
-        document.getElementById('lampada').style.backgroundImage = 'url("/assets/img/desligada.jpg")'
+        lampada.style.backgroundImage = 'url("/assets/img/desligada.jpg")'
         interruptorDesligado.setAttribute('style', 'background-color: white;')
         interruptorLigado.setAttribute('style', 'border: 2px solid white;')
 }
 
 const exibirHora = () => {
-        const horaAtual = `Horário de Brasília: ${hora.getHours()}:${hora.getMinutes()}`
+        const horaAtual = `Horário de Brasília: ${data.getHours()}:${data.getMinutes()}`
         idHora.textContent = horaAtual
-        idHora.style.color = black
+        idHora.style.color = 'black'
 }
 
 const verificarHora = () => {
-    const ehDia = hora.getHours() >= 6 && hora.getHours() < 18
+    const ehDia = data.getHours() >= 6 && data.getHours() < 18
 
     if(ehDia){
         return desligar()
