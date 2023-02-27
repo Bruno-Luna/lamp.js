4031b8b25db91917998d2e448fa1cbbdd3def735
Bruno-Luna <luna.brsilva@gmail.com>
2023-02-27 07:28:03 -0300

    refactor: caminho da imagem

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,13 +6,15 @@ let idHora = document.getElementById('hora')
 let data = new Date()
 
 const ligar = () => {
-        lampada.style.backgroundImage = 'url("/img/ligada.jpg")'
+        lampada.setAttribute('style', 'background-image: url("/img/ligada.jpg")')
+        // lampada.style.backgroundImage = ''
         interruptorLigado.setAttribute('style', 'background-color: white;')
         interruptorDesligado.setAttribute('style', 'border: 2px solid white;')
 }
-    
+
 const desligar = () => {
-        lampada.style.backgroundImage = 'url("/img/desligada.jpg")'
+        // lampada.style.backgroundImage = 'url("/img/desligada.jpg")'
+        lampada.setAttribute('style', 'background-image: url("/img/desligada.jpg")')
         interruptorDesligado.setAttribute('style', 'background-color: white;')
         interruptorLigado.setAttribute('style', 'border: 2px solid white;')
 }
